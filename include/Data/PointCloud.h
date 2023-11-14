5dfb4b853e893c1512538120b87283f5d21d6403
Alexandre <beaujonalexandre@gmail.com>
2023-11-14 20:38:31 +0100

    Point cloud data object

--- include/Data/PointCloud.h
+++ include/Data/PointCloud.h
@@ -1,10 +1,39 @@
 #pragma once
 
+#include <vector>
+
+#include "Vector.h"
+
 /**
  * @brief Storage class for point clouds.
  * 
  */
 class PointCloud
 {
+public:
+    /**
+     * @brief A point in a point cloud is represented by this data structure.
+     * 
+     */
+    struct Element
+    {
+        Vector3f position;
+        Vector3f normal;
+    };
+public:
+    PointCloud();
+    PointCloud(const PointCloud& other);
+    PointCloud(PointCloud&& other);
+    PointCloud& operator=(const PointCloud& other);
+    PointCloud& operator=(PointCloud&& other);
 
+    void reservePoints(size_t count);
+
+    const std::vector<Element>& getPointsData() const;
+    const Element* getRawPointsData() const;
+
+    void addPoint(const Element& element);
+
+private:
+    std::vector<Element> m_PointsPositions;
 };
--- src/Data/PointCloud.cpp
+++ src/Data/PointCloud.cpp
@@ -1,1 +1,44 @@
 #include "Data/PointCloud.h"
+
+PointCloud::PointCloud()
+{}
+
+PointCloud::PointCloud(const PointCloud& other)
+    : m_PointsPositions(other.m_PointsPositions)
+{}
+
+PointCloud::PointCloud(PointCloud&& other)
+    : m_PointsPositions(std::move(other.m_PointsPositions))
+{}
+
+PointCloud& PointCloud::operator=(const PointCloud& other)
+{
+    m_PointsPositions = other.m_PointsPositions;
+    return *this;
+}
+
+PointCloud& PointCloud::operator=(PointCloud&& other)
+{
+    m_PointsPositions = other.m_PointsPositions;
+    return *this;
+}
+
+void PointCloud::reservePoints(size_t count)
+{
+    m_PointsPositions.reserve(count);
+}
+
+const std::vector<PointCloud::Element>& PointCloud::getPointsData() const
+{
+    return m_PointsPositions;
+}
+
+const PointCloud::Element* PointCloud::getRawPointsData() const
+{
+    return m_PointsPositions.data();
+}
+
+void PointCloud::addPoint(const PointCloud::Element& element)
+{
+    m_PointsPositions.push_back(element);
+}
